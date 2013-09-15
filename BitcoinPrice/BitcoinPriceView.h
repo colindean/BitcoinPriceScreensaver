25541a31124158b2a82399c6b6cdc4702ae290ad
Colin Dean <git@cad.cx>
2013-09-15 01:41:16 -0400

    Initial Commit

---
+++ BitcoinPrice/BitcoinPriceView.h
@@ -0,0 +1,13 @@
+//
+//  BitcoinPriceView.h
+//  BitcoinPrice
+//
+//  Created by Colin Dean on 9/15/13.
+//  Copyright (c) 2013 Colin Dean. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface BitcoinPriceView : ScreenSaverView
+
+@end
---
+++ BitcoinPrice/BitcoinPriceView.m
@@ -0,0 +1,52 @@
+//
+//  BitcoinPriceView.m
+//  BitcoinPrice
+//
+//  Created by Colin Dean on 9/15/13.
+//  Copyright (c) 2013 Colin Dean. All rights reserved.
+//
+
+#import "BitcoinPriceView.h"
+
+@implementation BitcoinPriceView
+
+- (id)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
